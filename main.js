60b461a44e9568b476d9c866906470eccf148ca5
kuoricky22 <kuoricky22@gmail.com>
2023-08-31 01:56:31 +0800

    上傳第五周作業

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 import './assets/scss/all.scss';
 
+let current_page = window.location.href.split('/')[5].slice(0, window.location.href.split('/')[5].indexOf('.'));
+let navbar_links = document.getElementsByClassName('nav-link');
+
+
+Array.from(navbar_links).forEach(element => {
+    element.classList.remove('active');
+    let navbar_link = element.innerText.split("\n")[1].toLowerCase() === "admin" ? 'index' : element.innerText.split("\n")[1].toLowerCase();
+
+    if(navbar_link === current_page){
+        element.classList.add('active');
+    }
+  });
+
+
+
 console.log("Hello world!");
